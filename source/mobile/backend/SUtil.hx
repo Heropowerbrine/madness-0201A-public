d7a51b4e68a5758a1b1b7e592bfaded562886207
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-28 14:23:01 +0800

    And this too

--- source/mobile/backend/SUtil.hx
+++ source/mobile/backend/SUtil.hx
@@ -158,8 +158,8 @@ class SUtil
 enum abstract StorageType(String) from String to String
 {
 	final forcedPath = '/storage/emulated/0/';
-	final packageNameLocal = 'com.shadowmario.psychengineek';
-	final fileLocal = 'PsychEngineExtraKeys';
+	final packageNameLocal = 'com.shadowmario.madness';
+	final fileLocal = 'MADNESS';
 
 	var EXTERNAL_DATA = "EXTERNAL_DATA";
 	var EXTERNAL_OBB = "EXTERNAL_OBB";
