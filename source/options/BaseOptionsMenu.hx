4f62cd8fdbae8e74af69ac932bb882723930bc37
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-28 20:57:56 +0800

    Update BaseOptionsMenu.hx

--- source/options/BaseOptionsMenu.hx
+++ source/options/BaseOptionsMenu.hx
@@ -160,7 +160,11 @@ class BaseOptionsMenu extends MusicBeatSubstate
 		}
 
 		if (controls.BACK #if mobile || _virtualpad.buttonB.justPressed #end) {
+			#if mobile
+			closeSs();
+			#else
 			close();
+			#end
 			FlxG.sound.play(Paths.sound('madness/beep'));
 		}
 
