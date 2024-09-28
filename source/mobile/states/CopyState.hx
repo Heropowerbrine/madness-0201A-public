2aca56762658dbed5e09d3bc59006fd999739483
Hero <78842166+Heropowerbrine@users.noreply.github.com>
2024-09-28 16:54:31 +0800

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -177,7 +177,7 @@ class CopyState extends MusicBeatState
 		locatedFiles = OpenFLAssets.list();
 		
 		// removes unwanted assets
-		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/'));
+		locatedFiles = locatedFiles.filter(folder -> folder.startsWith('assets/'));
 		//var mods = locatedFiles.filter(folder -> folder.startsWith('mods/'));
 		//locatedFiles = assets.concat(mods);
 
